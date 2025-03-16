a8be998f64e6afb46255a493871b0f8fdeb7640e
Ulaş Aylar <ulasaylar@gmail.com>
2025-03-16 16:24:16 +0300

    css improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./css/App.css";
 import "./css/nav.css";
 import "./css/tech_stack.css";
+import "./css/about_me.css";
 import Nav from "./components/nav.jsx";
 import TechStack from "./components/tech_stack.jsx";
+import About_me from "./components/about_me.jsx";
 
 export default function App() {
   return (
     <>
       <Nav />
+      <About_me />
       <TechStack />
     </>
   );
---
+++ src/components/about_me.jsx
@@ -0,0 +1,22 @@
+const About_me = () => {
+  return (
+    <div className="about-me-container">
+      <div className="about-me-left-cont">
+        <h1>
+          Hi! I'm Ulaş <br></br> Welcome To My Portfolio Website!
+        </h1>
+        <p>
+          Thank you for visiting my website! This is a portfolio site that I’ve
+          designed and built entirely myself.
+          <br></br> I’m currently studying Computer Programming at İzmir
+          University of Economics.<br></br>Here, you’ll find some of the
+          projects I’ve worked on.<br></br>Feel free to explore my work, and
+          don’t hesitate to reach out if you’d like to collaborate or just have
+          a chat!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About_me;
